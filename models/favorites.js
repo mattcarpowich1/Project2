cea5bbda78ab03f8522f3892494fa11aed15e107
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2017-11-02 14:02:12 -0700

    started on favorites model

---
+++ models/favorites.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+
+	var Favorites = sequelize.define("Favorites", {}, {
+		freezeTableName: true
+	});
+
+	Favorites.associate = function(models) {
+		Favorites.belongsTo(models.Riffs, {
+			onDelete: "CASCADE",
+			foreignKey: {
+				allowNull: false
+			}
+		});
+		// Favorites.belongsTo(models.Users, {
+		// 	onDelete: "CASCADE",
+		// 	foreignKey: {
+		// 		allowNull: false
+		// 	}
+		// });
+	}
+
+	return Favorites;
+
+}
