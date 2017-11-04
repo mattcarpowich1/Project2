44d8f4f7ddf76e41c97ec69530ac9c6519a2a541
Saam Khosravi <khosravi.saam@gmail.com>
2017-11-04 10:25:30 -0700

    adding files and routes for user login

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,10 @@
-let express = require("express");
-let request = require("request");
+let express = require('express');
+let request = require('request');
+let passport = require('passport');
+let Strategy = require('passport-local').Strategy;
 
 let router = express.Router();
-let db = require("../models");
+let db = require('../models');
 
 // =======================================
 //               GET ROUTES
@@ -13,9 +15,9 @@ let db = require("../models");
  * @param  {[type]} req [HTTP Request]
  * @param  {[type]} res [HTTP Response, renders index.ejs]
  */
-router.get("/", function(req, res) {
-  db.Riffs.findAll({}).then(function(allRiffs) {
-    res.render("pages/index", {riffs: allRiffs});
+router.get('/', function (req, res) {
+  db.Riffs.findAll({}).then(function (allRiffs) {
+    res.render('pages/index', {riffs: allRiffs, user: req.user});
   });
 });
 
@@ -24,9 +26,9 @@ router.get("/", function(req, res) {
  * @param  {[type]} req [HTTP Request]
  * @param  {[type]} res [HTTP Response, renders login.ejs]
  */
-router.get("/login", function(req, res) {
-  db.Riffs.findAll({}).then(function(allRiffs) { //change to users table when available?
-    res.render("pages/login", {riffs: allRiffs});
+router.get('/login', function (req, res) {
+  db.Riffs.findAll({}).then(function (allRiffs) { // change to users table when available?
+    res.render('pages/login', {riffs: allRiffs, user: req.user});
   });
 });
 
@@ -35,9 +37,9 @@ router.get("/login", function(req, res) {
  * @param  {[type]} req [HTTP Request]
  * @param  {[type]} res [HTTP Response, renders user.ejs]
  */
-router.get("/user", function(req, res) {
-  db.Riffs.findAll({}).then(function(allRiffs) { //change to users table when available
-    res.render("pages/user", {riffs: allRiffs});
+router.get('/user', function (req, res) {
+  db.Riffs.findAll({}).then(function (allRiffs) { // change to users table when available
+    res.render('pages/user', {riffs: allRiffs, user: req.user});
   });
 });
 
@@ -46,8 +48,8 @@ router.get("/user", function(req, res) {
  * @param  {Object} req [HTTP Request]
  * @param  {Object} res [HTTP Response, returns JSON]
  */
-router.get("/api/riffs/all", function(req, res) {
-  db.Riffs.findAll({}).then(function(allRiffs) {
+router.get('/api/riffs/all', function (req, res) {
+  db.Riffs.findAll({}).then(function (allRiffs) {
     res.json(allRiffs);
   });
 });
@@ -57,12 +59,12 @@ router.get("/api/riffs/all", function(req, res) {
  * @param  {Object} req [HTTP Request]
  * @param  {Object} res [HTTP Response, returns JSON upon succesful query]
  */
-router.get("/api/riffs/:id", function(req, res) {
+router.get('/api/riffs/:id', function (req, res) {
   db.Riffs.findOne({
     where: {
       id: req.params.id
     }
-  }).then(function(oneRiff) {
+  }).then(function (oneRiff) {
     res.json(oneRiff);
   });
 });
@@ -76,7 +78,7 @@ router.get("/api/riffs/:id", function(req, res) {
  * @param  {Object} req [HTTP Request, contains riff object in post body]
  * @param  {Object} res [HTTP Response, returns new entry as JSON upon successful query]
  */
-router.post("/api/riffs/new", function(req, res) {
+router.post('/api/riffs/new', function (req, res) {
   let j = JSON.stringify(req.body, 2, null);
   console.log(j);
   // db.Riffs.create(req.body).then(function(newRiff) {
@@ -84,4 +86,25 @@ router.post("/api/riffs/new", function(req, res) {
   // });
 });
 
+// =======================================
+//             USER ROUTES
+// =======================================
+
+router.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/');
+  });
+
+router.get('/logout',
+  function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+router.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function (req, res) {
+    res.render('pages/profile', { user: req.user });
+  });
 module.exports = router;
