20b832e8496089060e288f125eb00b18224a6d53
Michael <contact@michaelmlowe.com>
2017-11-04 11:15:22 -0700

    Integrated Passport With Sequelize

---
+++ models/users.js
@@ -0,0 +1,74 @@
+module.exports = function(sequelize, DataTypes) {
+  var Users = sequelize.define("Users", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+        // custom validation to ensure that value is a string
+        isValidString: function(value) {
+          if (!(typeof value === "string")) {
+            throw new Error("username field must be a string!");
+          } else {
+            return true;
+          }
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+        // custom validation to ensure that value is a string
+        isValidString: function(value) {
+          if (!(typeof value === "string")) {
+            throw new Error("password field must be a string!");
+          } else {
+            return true;
+          }
+        }
+      }
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+        // custom validation to ensure that value is a string
+        isValidString: function(value) {
+          if (!(typeof value === "string")) {
+            throw new Error("displayName field must be a string!");
+          } else {
+            return true;
+          }
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+        // custom validation to ensure that value is a string
+        isValidString: function(value) {
+          if (!(typeof value === "string")) {
+            throw new Error("email field must be a string!");
+          } else {
+            return true;
+          }
+        }
+      }
+    }
+  });
+
+
+  //Users.hasMany(Riffs); 
+  // RELATIONSHIPS GO here
+  // Riff.belongsTo(Author);
+
+  return Users;
+};
+
+// in Authors model
+// Author.hasMany(Riff);
