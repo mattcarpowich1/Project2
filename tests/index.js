852dc3d416258eabb66b51217b4d47248882005a
Matthew Carpowich <matthewcarpowich@calvisitor-10-105-161-219.calvisitor.1918.berkeley.edu>
2017-11-09 17:51:22 -0800

    added some tests

---
+++ tests/index.js
@@ -0,0 +1,55 @@
+const Nightmare = require('nightmare');
+const nightmare = new Nightmare();
+
+// click the new riff area, enter C3 in the first step
+// should log 'C3'
+nightmare
+	.goto('http://localhost:3000')
+	.click('article:nth-child(1)')
+	.wait('.modal-card')
+	.click('.modal-step:nth-child(1)')
+	.click('tbody > tr > td:nth-child(3)')
+	.evaluate(() => {
+		return document.getElementById('modal-step-0').value;
+	})
+	.end()
+	.then(text => {
+		console.log(text);
+	})
+	.catch(err => {
+		console.log(err);
+	});
+
+const nightmare2 = new Nightmare();
+
+nightmare2
+	.goto('http://localhost:3000')
+	.click('#navbar-login-button')
+	.wait('.has-text-grey > a')
+	.click('.has-text-grey > a')
+	.wait('.box > form')
+	.type('#username', 'username')
+	.type('#displayName', 'test user')
+	.type('#email', 'test@gmail.com')
+	.type('#password', 'password')
+	.click('#submit-button')
+	.wait('#navbar-logo')
+	.click('#navbar-logo')
+	.wait('article')
+	.click('article:nth-child(1)')
+	.wait('#modal-title-input')
+	.type('#modal-title-input', 'Test Riff')
+	.click('#publish-riff')
+	.wait('section')
+	.click('.tile')
+	.wait('#modal-title-input')
+	.evaluate(() => {
+		return document.getElementById('modal-title-input');
+	})
+	.end()
+	.then(text => {
+		console.log(text);
+	})
+	.catch(err => {
+		console.log(err);
+	});
